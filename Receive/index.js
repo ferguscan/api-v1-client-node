c594eb97b2f45182e96d3f1f9acb5c716797b40a
Stuart Clarke <iamstuartclarke@gmail.com>
2018-10-30 16:35:51 +1000

    Added balance update functionality

--- Receive/index.js
+++ Receive/index.js
@@ -4,6 +4,7 @@ var UrlPattern = require('url-pattern')
 
 var endpoints = {
   receive: new UrlPattern('/receive?xpub=:xpub&callback=:callback&key=:key(&gap_limit=:gapLimit)'),
+  balanceupdate: new UrlPattern('/receive/balance_update'),
   checkgap: new UrlPattern('/receive/checkgap?xpub=:xpub&key=:key')
 }
 
@@ -23,6 +24,19 @@ Receive.prototype.generate = function (query) {
   var callbackEnc = encodeURIComponent(callbackUrl)
   var params = { xpub: this.xpub, key: this.key, callback: callbackEnc, gapLimit: this.gapLimit }
   return api.request('receive', params)
+}
+
+Receive.prototype.balanceupdate = function (addr, query) {
+  var callbackUrl = parse(this.callback).set('query', query).toString()
+  var data = {
+    key: this.key,
+    callback: callbackUrl,
+    addr,
+    onNotification: 'KEEP',
+    op: 'RECEIVE',
+    confs: 3
+  }
+  return api.post('balanceupdate', {}, JSON.stringify(data))
 }
 
 Receive.prototype.checkgap = function () {
